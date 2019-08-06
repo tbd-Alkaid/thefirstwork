cc52b10fc929af9446315043f8c6a71f1aa293bc
tbd-Alkaid <wangyunzhuo95@gmail.com>
2019-08-06 09:41:10 +0800

    Initial commit

---
+++ config.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+class Config(object):
+    n_layer = 4
+    batch_size = 2
+    valid_size = 2
+    step_boundaries = [2000, 4000]
+    num_iterations = 6000
+    logging_frequency = 5
+    verbose = True
+    y_init_range = [0, 1]
+
+
+class AllenCahnConfig(Config):
+    total_time = 0.3
+    num_time_interval = 2
+    dim = 5
+    lr_values = list(np.array([5e-4, 5e-4]))
+    lr_boundaries = [2000]
+    num_iterations = 1
+    num_hiddens = [dim, dim + 10, dim + 10, dim]
+    y_init_range = [0.3, 0.6]
+
+def get_config(name):
+    try:
+        return globals()[name+'Config']
+    except KeyError:
+        raise KeyError("Config for the required problem not found.")
---
+++ main.py
@@ -0,0 +1,44 @@
+"""
+The main file to run BSDE solver to solve parabolic partial differential equations (PDEs).
+
+"""
+
+import json
+import logging
+import os
+import numpy as np
+import tensorflow as tf
+from config import get_config
+from equation import get_equation
+from solver import FeedForwardModel
+
+
+def main():
+    problem_name0 = 'AllenCahn'
+    config = get_config(problem_name0)
+    bsde = get_equation(problem_name0, config.dim, config.total_time, config.num_time_interval)
+    # log_dir = './logs'
+    # if not os.path.exists(log_dir):
+    #     os.mkdir(log_dir)
+    # path_prefix = os.path.join(log_dir, problem_name0)
+    # with open('{}_config.json'.format(path_prefix), 'w') as outfile:
+    #     json.dump(dict((name, getattr(config, name))
+    #                    for name in dir(config) if not name.startswith('__')),
+    #               outfile, indent=2)
+    # logging.basicConfig(level=logging.INFO, b
+    #                     format='%(levelname)-6s %(message)s')
+
+    for idx_run in range(1, 2):
+        tf.reset_default_graph()
+        with tf.Session() as sess:
+            # logging.info('Begin to solve %s with run %d' % (problem_name0, idx_run))
+            model = FeedForwardModel(config, bsde, sess)
+            # if bsde.y_init:
+            #     logging.info('Y0_true: %.4e' % bsde.y_init)
+            model.build()
+            training_history = model.train()
+            #print(training_history)
+            writer = tf.summary.FileWriter('./graph/ograph',sess.graph)
+
+if __name__ == '__main__':
+    main()
